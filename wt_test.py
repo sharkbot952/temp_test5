aae4f325065d979ed7f6e14ac2556f0a89e0a6fe
sharkbot952 <231409878+sharkbot952@users.noreply.github.com>
2026-04-12 15:38:30 +0900

    update

--- wt_test.py
+++ wt_test.py
@@ -1440,9 +1440,8 @@ elif view_mode == "水温図":
             df_period.sort_values("datetime")
             .groupby("depth_m", group_keys=False)
             .apply(lambda g: (
-                g.drop(columns=["depth_m"], errors="ignore")
-                .set_index("datetime")
-                .resample("1H").median(numeric_only=True).interpolate(method="time", limit=2).reset_index()
+                g.drop(columns=["depth_m"], errors="ignore").set_index("datetime")
+                .resample("1h").median(numeric_only=True).interpolate(method="time", limit=2).reset_index()
                 .assign(depth_m=int(g.name) if g.name is not None else pd.NA)
             ))
         )
@@ -1473,7 +1472,7 @@ elif view_mode == "水温図":
                 .apply(lambda g: (
                     g.drop(columns=["depth_m"], errors="ignore")
                     .set_index("datetime")[use_cols]
-                    .resample("1H").median().dropna(how="all").reset_index()
+                    .resample("1h").median().dropna(how="all").reset_index()
                     .assign(depth_m=int(g.name) if g.name is not None else pd.NA)
                 ))
             )
